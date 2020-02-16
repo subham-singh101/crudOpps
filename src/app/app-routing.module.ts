9df15a135f936c6ad4e45f67c1f3db436c956518
subham-singh101 <subham.singh@meltwater.com>
2020-02-16 11:19:14 +0530

    frontend work done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddStudentComponent } from "./components/add-student/add-student.component";
+import { EditStudentComponent } from "./components/edit-student/edit-student.component";
+import { StudentsListComponent } from "./components/students-list/students-list.component";
+import { from } from 'rxjs';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', pathMatch : 'full', redirectTo : 'add-student' },
+  { path : 'add=student', component : AddStudentComponent },
+  { path : 'edit-student/:id', component : EditStudentComponent },
+  { path : 'student-list', component : StudentsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { StudentsListComponent } from './components/students-list/students-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* ANGULAR MATERIAL */
+import { AngularMaterialModule } from './material.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    StudentsListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
